8328f5b5d5900d29386700eaa76d4ce05720a1a8
윤영주 <user@yun-yeongjuui-MacBookAir.local>
2022-01-26 00:40:16 +0900

    feat(user.ts): post로 값을 받아 db에 저장합니다.

--- src/back/routes/user.ts
+++ src/back/routes/user.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connection } from '../service';
 import type { Router, Request, Response, NextFunction } from 'express';
+import { MysqlError } from 'mysql';
 const log = console.log;
 
 const userRouter: Router = express.Router();
@@ -11,6 +12,32 @@ userRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 userRouter.post('/', (req: Request, res: Response): void => {
   connection.query('USE my_base_db');
+  connection.query(
+    `CREATE TABLE user_comment(
+    user_comment_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    comment VARCHAR(30) NOT NULL
+  );`,
+    function (error: MysqlError): void {
+      if (error) {
+        console.log('user_comment table is already exist!');
+        connection.query(
+          `INSERT INTO user_comment(comment)
+        VALUES ('${req.body.comment}');`,
+          function (error: MysqlError, result) {
+            console.log('insert into result', result);
+            return result;
+          }
+        );
+      }
+      console.log(`user_comment table created!`);
+    }
+  );
+  connection.query(
+    `SELECT * FROM my_base_db.users`,
+    function (error: MysqlError, result) {
+      console.log('user result: ', result);
+    }
+  );
   res.status(201).json(req.body);
 });
 export default userRouter;
